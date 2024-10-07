e8e6ee222df3e3889bd9190a285c782e7df545dc
ClaudioB12 <119316401+ClaudioB12@users.noreply.github.com>
2024-10-07 09:36:40 -0500

    seguridad en mc

--- ms-gateway-server/src/main/java/com/example/msgatewayserver/dto/TokenDto.java
+++ ms-gateway-server/src/main/java/com/example/msgatewayserver/dto/TokenDto.java
@@ -4,14 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @NoArgsConstructor
-
-
 @Builder
 @AllArgsConstructor
 @Data
 public class TokenDto {
     private String token;
 }
-
